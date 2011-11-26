fdd089b47ee3a76904d4109f440375d55ace91c7
Hery <aeonzamaek@gmail.com>
2011-11-26 15:24:06 -0500

    wired up zipcode submit button

--- historicalWeather/src/com/daemgahe/historicalWeather/HistoricalWeatherActivity.java
+++ historicalWeather/src/com/daemgahe/historicalWeather/HistoricalWeatherActivity.java
@@ -2,12 +2,29 @@ package com.daemgahe.historicalWeather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class HistoricalWeatherActivity extends Activity {
+public class HistoricalWeatherActivity extends Activity 
+{
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+		
+        ((Button) findViewById(R.id.submit1)).setOnClickListener
+        (
+        		new Button.OnClickListener() 
+        		{
+        			@Override public void onClick(View arg0) 
+        			{
+        				// Process data here
+        				// setContentView(R.layout.date);
+        			}
+        		}
+        );			
     }
 }
