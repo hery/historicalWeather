afb34d358d3cf4383e82c9f68cfc080e6c4d0cba
Gaurav Munjal <Gaurav0@aol.com>
2011-11-26 18:09:03 -0500

    Rebuild after merge
    Signed-off-by: Gaurav Munjal <Gaurav0@aol.com>

--- historicalWeather/gen/com/daemgahe/historicalWeather/R.java
+++ historicalWeather/gen/com/daemgahe/historicalWeather/R.java
@@ -13,11 +13,17 @@ public final class R {
     public static final class drawable {
         public static final int weather_temp=0x7f020000;
     }
+    public static final class id {
+        public static final int submit1=0x7f050000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040002;
+        public static final int dateString=0x7f040001;
+        public static final int resultingChartString=0x7f040004;
+        public static final int submit=0x7f040003;
+        public static final int zipString=0x7f040000;
     }
 }
