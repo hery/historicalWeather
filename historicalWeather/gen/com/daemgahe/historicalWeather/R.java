4048e8e9ee3eae77b82ce38ad21a870104b98ec7
Hery <aeonzamaek@gmail.com>
2011-12-16 21:15:35 -0500

    added wu logo

--- historicalWeather/gen/com/daemgahe/historicalWeather/R.java
+++ historicalWeather/gen/com/daemgahe/historicalWeather/R.java
@@ -11,7 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int weather_temp=0x7f020000;
+        public static final int rev_bw=0x7f020000;
+        public static final int weather_temp=0x7f020001;
     }
     public static final class id {
         public static final int debugTextView=0x7f050006;
